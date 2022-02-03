b7226ec59e93a35ad8ba85d995b1dfa65ac1c11b
carlllcanlas <83762463+carlllcanlas@users.noreply.github.com>
2022-02-03 22:40:23 +0800

    Add files via upload

---
+++ Lab 5/test-bind.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-bind',
+  templateUrl: './test-bind.component.html',
+  styleUrls: ['./test-bind.component.css']
+})
+export class TestBindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+//card
+studName=""
+section=""
+subject=""
+prelim=0
+final=0
+midterm=0
+studentNumber=0;
+
+//card2
+studName1=""
+section1=""
+subject1=""
+prelim1=0
+final1=0
+midterm1=0
+studentNumber1=0;
+course1=""
+
+}
