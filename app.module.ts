b710eb399c6335abd6abf50fd695d99e688b16e3
carlllcanlas <83762463+carlllcanlas@users.noreply.github.com>
2022-02-03 22:25:42 +0800

    PrelimActivity

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { GradebookComponent } from './gradebook/gradebook.component';
+import { TestBindComponent } from './test-bind/test-bind.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GradebookComponent,
+    TestBindComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
